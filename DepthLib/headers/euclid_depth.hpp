c7987403702f6bf5cbf84ba179fd9bccb329fd0b
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2014-09-14 00:40:05 +0200

    Euclidean depth functions.

---
+++ DepthLib/headers/euclid_depth.hpp
@@ -0,0 +1,23 @@
+/*
+ * euclid_depth.hpp
+ *
+ *  Created on: Sep 14, 2014
+ *      Author: zzawadz
+ */
+
+#ifndef EUCLID_DEPTH_HPP_
+#define EUCLID_DEPTH_HPP_
+
+#include "DepthLib_include.hpp"
+
+namespace depth
+{
+
+arma::vec euclid_depth(const arma::mat x, arma::rowvec center);
+arma::vec euclid_depth(const arma::mat& x, const arma::mat& y);
+arma::vec euclid_depth(const arma::mat& x);
+
+}
+
+
+#endif /* EUCLID_DEPTH_HPP_ */
